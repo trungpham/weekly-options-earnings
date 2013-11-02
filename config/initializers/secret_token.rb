b9343248d15201cd1314d3617a22009d256f5995
Trung Pham <trung@phamcom.com>
2013-11-02 16:53:02 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Earnings::Application.config.secret_key_base = 'f8b98456cf01ee96f42db80b3dcc73777ed86a290e5e20d4195529e917d0424097b6c1411d02b54946e78d79ae61189b267029b701ad2e487908fdffd60ce3ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Earnings::Application.config.session_store :cookie_store, key: '_earnings_session'
